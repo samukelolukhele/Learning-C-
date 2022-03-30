7970bcf11e57429b6646f120da80a96a8f45258f
Samukel Lukhele <kelo.lukhele@gmail.com>
2022-03-30 14:54:34 +0200

    First few lines of C#

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Martian";
+            int age  = 21;
+            Console.WriteLine("{0} is {1}", name, age);
+        }
+    }
+}
