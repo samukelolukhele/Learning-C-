3bc598aa87db26d9e71f44dadf347cfc4fda58cd
Samukel Lukhele <kelo.lukhele@gmail.com>
2022-03-30 15:38:02 +0200

    Number guesser program

--- Program.cs
+++ Program.cs
@@ -6,9 +6,106 @@ namespace helloworld
     {
         static void Main(string[] args)
         {
-            string name = "Martian";
-            int age  = 21;
-            Console.WriteLine("{0} is {1}", name, age);
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Samukelo Lukhele";
+
+            // Change text color
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //Write out app info
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            //Reset text color
+            Console.ResetColor();
+
+            //Ask users name
+
+            Console.WriteLine("What is your name");
+
+            //Get user input
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, let's play a game...", inputName);
+
+            while (true)
+            {
+
+                //Create a new random object
+
+                Random random = new Random();
+
+                //Init correct number
+                int correctNumber = random.Next(1, 10);
+
+                // Init guess var
+                int guess = 0;
+
+                //Ask user for number
+                Console.WriteLine("Guess a number between 1 and 10");
+
+                // While guess is not correct
+                while (guess != correctNumber)
+                {
+                    //Get users input 
+                    string input = Console.ReadLine();
+
+                    //Make sure its a number
+                    if (!int.TryParse(input, out guess))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        // Tell user its the wrong number
+                        Console.WriteLine("Please enter a valid number.");
+
+                        Console.ResetColor();
+
+                        continue;
+                    }
+
+                    // Cast to int and put in guess
+                    guess = Int32.Parse(input);
+
+                    // Match guess to correct number
+
+                    if (guess != correctNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        // Tell user its the wrong number
+                        Console.WriteLine("Wrong number, please try again");
+
+                        Console.ResetColor();
+                    }
+                }
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                // Tell user its the right number
+                Console.WriteLine("Correct, you win!");
+
+                Console.ResetColor();
+
+                //Ask to play again
+
+                Console.WriteLine("Play again? [Y or N]");
+
+                //Get answer
+                string answer = Console.ReadLine().ToUpper();
+
+                if (answer == "Y")
+                {
+                    continue;
+                }
+                else if (answer == "N")
+                {
+                    return;
+                }
+                else
+                {
+                    return;
+                }
+            }
         }
     }
 }
